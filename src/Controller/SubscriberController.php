88b40a8a339d5ebb182001e025a660d2405d776a
Waqar Hussain <waqar.fullstack.dev@gmail.com>
2022-08-23 21:02:08 +0200

    initial

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -2,7 +2,41 @@
 
 namespace App\Controller;
 
-class SubscriberController
+use App\Entity\Subscriber;
+use App\Form\SubscriberFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SubscriberController extends AbstractController
 {
+    /**
+     * @Route("/show")
+     */
+    public function show(Environment $twig, Request $request, EntityManagerInterface $entityManager)
+    {
+        $subscriber = new Subscriber();
+
+        $form = $this->createForm(SubscriberFormType::class, $subscriber);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $entityManager->persist($subscriber);
+            $entityManager->flush();
+
+            return new Response('New Subscriber '.$subscriber->getId().' is created');
+
+        }
+
+        return new Response($twig->render('subscriber/show.html.twig',[
+            'subscriber_form' => $form->createView()
+        ]));
+
+    }
 
 }
